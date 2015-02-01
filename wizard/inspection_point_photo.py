793d9e49ae401aa26b2640e7e1f2cfedb6cb43c2
theprovensupport <support@theproventechnology.com>
2015-02-01 20:29:17 +0100

    Inspection Point Photo changes

--- wizard/inspection_point_photo.py
+++ wizard/inspection_point_photo.py
@@ -5,6 +5,8 @@ class wo_task_inspection_photo(osv.osv_memory):
     _name = 'wo.task.inspection.photo'
     _description = 'Inspection Point Photo'
 
+    insPhoto = ""
+    
     def get_inspection_point_photo(self, cr, uid, context=None):
         if context is None:
             context = {}
@@ -12,8 +14,8 @@ class wo_task_inspection_photo(osv.osv_memory):
         inspection_point = self.pool.get('spec.inspection.points').browse(cr, uid, context.get('active_ids', [])[0])
         if inspection_point.image:
             return inspection_point.image
-        else:
-            raise osv.except_osv(_('Invalid Action!'), _("No Inspection image available !"))
+        #else:
+         #   raise osv.except_osv(_('Invalid Action!'), _("No Inspection image available !"))
 
     _columns = {
         'inspection_point_id': fields.many2one('spec.inspection.points', 'Inspection Point'),
@@ -24,4 +26,23 @@ class wo_task_inspection_photo(osv.osv_memory):
         'photo': get_inspection_point_photo,
     }
     
+    
+    
+    def onchange_add_inspection_image(self, cr, uid, ids, photo):
+        self.insPhoto = ""
+        if photo:
+            self.insPhoto = photo        
+        return True
+    
+    def add_inspection_image(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        sip_obj = self.pool.get('spec.inspection.points')
+         
+        if self.insPhoto:
+            #raise osv.except_osv(_('Invalid Action!'), _(str(self.insPhoto)))
+            sip_obj.write(cr, uid, context.get('active_ids', [])[0],{'image': self.insPhoto},context=context)
+        else:
+            sip_obj.write(cr, uid, context.get('active_ids', [])[0],{'image': ""},context=context)
+        return True
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
